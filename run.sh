b2826ca61725ee97fec11cadd5c42752766be64a
zero <cgddgc@foxmail.com>
2017-10-18 16:51:56 +0800

    1

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cd `dirname $0`
 python_ver=$(ls /usr/bin|grep -e "^python[23]\.[1-9]\+$"|tail -1)
 eval $(ps -ef | grep "[0-9] ${python_ver} server\\.py m" | awk '{print "kill "$2}')
 ulimit -n 512000
-nohup ${python_ver} server.py m>> /dev/null 2>&1 &
+nohup ${python_ver} server.py m>>/var/log/ssr/ssr.log 2>>/var/log/ssr/error.log &
 
